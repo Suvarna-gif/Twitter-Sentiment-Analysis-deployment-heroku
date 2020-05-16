a1a4565edad5f5e6ba79f9172148d2ade0078f1e
Suvarna-gif <62047925+Suvarna-gif@users.noreply.github.com>
2020-05-16 20:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def tweets():
         return jsonify({'data': tweets, 'count': len(tweets)})
 
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host="127.0.0.1", port=port, debug=True)
+if __name__ == '__main__':
+    app.run()
